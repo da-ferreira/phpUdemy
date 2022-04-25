a20d293fb00897a196a27ac4f4fdfc367eed71d9
David Ferreira <ferreiradavid361@gmail.com>
2022-04-25 16:29:15 -0300

    templates: header (cabeçalho) e footer (rodapé)

--- 11_blog/contato.php
+++ 11_blog/contato.php
@@ -2,7 +2,7 @@
 <?php
 include_once("templates/header.php");
 ?>
-    <h1>Teste</h1>
+    <h1>Página de contato</h1>
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ 11_blog/templates/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>Hora de Codar &copy; 2022</p>  <!-- Colocar logo aqui  -->
+    </footer>
+</body>
+</html>
